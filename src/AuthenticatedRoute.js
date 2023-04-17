ae37b5889d1097a1af5f7e508995f6036cd74355
moin105 <moinlatif105@gmail.com>
2023-04-18 02:44:23 +0500

    lkhfdsa

--- src/AuthenticatedRoute.js
+++ src/AuthenticatedRoute.js
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
 import { Outlet, Navigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const AuthenticatedRoute = ({ isAuthenticated }) => {
+  const navigate = useNavigate();
+  // Function to handle route change
+  const handleRouteChange = (route) => {
+    // Use the navigate() function to navigate to the specified route
+    navigate(route);
+  };
+  useEffect(() => {
+  if(token){
+    handleRouteChange('/home')
+  }
+  }, [])
   if (!isAuthenticated) {
     return <Navigate to="/login" replace />;
   }
