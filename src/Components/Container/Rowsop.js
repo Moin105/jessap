2dd99616078d7a21939470531fee19ad7fa8dd0a
moin105 <moinlatif105@gmail.com>
2023-04-25 09:32:59 +0500

    werty

--- src/Components/Container/Rowsop.js
+++ src/Components/Container/Rowsop.js
@@ -1,70 +1,106 @@
 import React, { useState } from "react";
-import { useNavigate } from 'react-router-dom';
-import {AiFillCloseCircle} from 'react-icons/ai'
-import dots from '../../Images/dots.png'
+import { useNavigate } from "react-router-dom";
+import { AiFillCloseCircle } from "react-icons/ai";
+import dots from "../../Images/dots.png";
+// import { useDispatch } from "react-redux";
+import { fetchData } from "./Sops";
 import Cookies from "js-cookie";
+import { useDispatch } from "react-redux";
 
-
-
-
-function Rowsop({icon,title,description,data}) {
- const token = Cookies.get("token")
-  const [show, setShow] = useState(false)
+function Rowsop({ icon, title, description, data }) {
+  const checkForSuccessfull = (str) => {
+    return str.includes("SOP deleted successfully");
+  };
+  const dispatch = useDispatch();
+  const token = Cookies.get("token");
+  const [show, setShow] = useState(false);
   const navigate = useNavigate();
-  const {id} = data;
-  // Function to handle route change
- const deleteSop = async ()=>{
-  console.log("naxxr",id);
-  fetch(`https://phplaravel-391561-3408566.cloudwaysapps.com/api/SOPs/${id}`, {
-    method: "Delete",
+  const { id } = data;
+  const config = {
     headers: {
-      "Content-Type": "application/json",
-      Authorization: `Bearer ${token}`,
-    },
-    // body: JSON.stringify(employee),ss
-  })
-  .then(response=>{return response.json()})
-  .then(response => console.log(response))
- }
+      'Authorization':`Bearer  ${Cookies.get("token")}`
+    }
+  };
+  // Function to handle route change
+  const deleteSop = async () => {
+    console.log("naxxr", id);
+    fetch(
+      `https://phplaravel-391561-3408566.cloudwaysapps.com/api/SOPs/${id}`,
+      {
+        method: "Delete",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${token}`,
+        },
+        // body: JSON.stringify(employee),ss
+      }
+    )
+      .then((response) => {
+        return response.json();
+      })
+      .then((response) => {
+        // if (response.message == "SOP deleted successfully") {
+        //   dispatch(fetchData(config));
+        // }
+        const isPresent = checkForSuccessfull(response.message);
+        //   "Company Registered successfully."
+        console.log(isPresent);
+        if (isPresent) {
+          dispatch(fetchData(config));
+          setShow(false);
+          // handleRouteChange("/home");
+        }
+        console.log("sajna", response);
+      });
+  };
+
   const dataas = {
-    message: 'Hello from Component A',
-    sop:data,
+    message: "Hello from Component A",
+    sop: data,
     // Add more data as needed
   };
-const handleButton = ()=>{
-  console.log("sop",id)
-  deleteSop()
-}
+  const handleButton = () => {
+    console.log("sop", id);
+    deleteSop();
+  };
   const handleClick = (id) => {
     navigate(`/dynamic/${id}`, { state: { dataas } });
   };
   return (
     <React.Fragment>
-      <div  className="row"  >
+      <div className="row">
         <div className="box">
-            <div className="left" onClick={()=>{handleClick(id)}}>
-   <div className="icon-container">
-            <img src={icon} />
-          </div>
-          <div className="detail-heading">
-            <h5 >{title}</h5>
-            <p>{description}</p>
-          </div>
+          <div
+            className="left"
+            onClick={() => {
+              handleClick(id);
+            }}
+          >
+            <div className="icon-container">
+              <img src={icon} />
             </div>
-       
-         <div className="circle-box">
+            <div className="detail-heading">
+              <h5>{title}</h5>
+              <p>{description}</p>
+            </div>
+          </div>
+
+          <div className="circle-box">
             <div className="circle">{/* <img>?</img> */}</div>
             <div className="circle">{/* <img>?</img> */}</div>
             <div className="circle">{/* <img>?</img> */}</div>
-         </div> 
+          </div>
         </div>
-     {show &&<div className="hidden-box">
+        {show && (
+          <div className="hidden-box">
             {/* <AiFillCloseCircle/> */}
             <button onClick={handleButton}>Delete SOP</button>
-      </div>}
-        <span className="dots" onClick={()=>setShow(!show)}><img src={dots}/></span>
+          </div>
+        )}
+        <span className="dots" onClick={() => setShow(!show)}>
+          <img src={dots} />
+        </span>
       </div>
-   
     </React.Fragment>
   );
 }
--- src/Components/Container/Sops.js
+++ src/Components/Container/Sops.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react'
+import React, { useEffect,useState } from 'react'
 import Rowsop from './Rowsop'
 import setting from "../../Images/setting.png";
 import check from "../../Images/check.png";
@@ -43,7 +43,31 @@ export const fetchData = (config) => {
 
 function Sops() {
   const dispatch = useDispatch();
+  const [searchQuery, setSearchQuery] = useState('');
+  
   const sop = useSelector(state => state.sops);
+  const filteredSops = searchQuery ? sop.data.filter(sop => sop.title.toLowerCase().includes(searchQuery.toLowerCase())) : sop.data;
+  // let content;
+
+  // if (filteredSops.length > 0) {
+  //   content = (
+  //     <ul>
+  //       {filteredSops.data.map(sop => <li key={sop.id}>{sop.title}</li>)}
+  //     </ul>
+  //   );
+  // } else if (searchQuery && filteredSops.data.length === 0) {
+  //   content = (
+  //     <p>No SOPs found.</p>
+  //   );
+  // } else {
+  //   content = (
+  //     <ul>
+  //       {sop.data.map(sop => <li key={sop.id}>{sop.title}</li>)}
+  //     </ul>
+  //   );
+  // }
+  // const filteredSops = searchQuery ? sop.filter(sop => sop.title.toLowerCase().includes(searchQuery.toLowerCase())) : sop;
+
   // const token ={Authorization:}
   const navigate = useNavigate();
   const config = {
@@ -54,12 +78,21 @@ function Sops() {
   const handleClick = (id,data) => {
     navigate(`/dynamic/${id}`);
   };
-useEffect(() => {
-  console.log("container",sop)
-  dispatch(fetchData(config));
+  
+  useEffect(() => {
+    console.log("container",sop)
+    dispatch(fetchData(config));
+  }, [])
+  // let filteredSops = sop;
 
-}, [])
-
+ 
+  // useEffect(() => {
+  // console.log("jawab",filteredData)
+  // if (Array.isArray(sop)) {
+  //   filteredSops = searchQuery ? sop.data.filter(sop => sop.title.toLowerCase().includes(searchQuery.toLowerCase())) : sop;
+  // }
+  // }, [searchQuery])
+  
   return (
    <React.Fragment>
              <div className='container-sop'>
@@ -68,19 +101,36 @@ useEffect(() => {
             <div className='sop-container-row'>
               <div className='input-container'>
                 <span><CgSearch/></span>
-                <input type='text' placeholder='Search SOPs'/>
+                <input type="text" value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)} />
+                {/* <input type='text'  value={searchQuery} onChange={(e) =>{ setSearchQuery(e.target.value);console.log("e",searchQuery)}} placeholder='Search SOPs'/> */}
               </div>
               <Link to="/addsops"><button>Add SOP</button></Link>  
             </div>
             <div className='card-sop'>
-              {sop !== [] ? sop.data.map((sops,index )=>{
-               return ( <Rowsop data={sops}  icon={setting}title={sops.title} description={sops.description}/>
-)              }):"no data"  }
+            {filteredSops.length > 0
+  ? <>{filteredSops.map((sops,index) => <Rowsop data={sops} key={index}  icon={setting}title={sops.title} description={sops.description}/>)}</>
+  : searchQuery && filteredSops.length === 0
+    ? <p>No SOPs found.</p>
+    : <>{sop.data.map((sops,index) => <Rowsop data={sops} key={index}  icon={setting}title={sops.title} description={sops.description}/>)}</>}
+            {/* {filteredSops.length > 0 ? (
+      <ul>
+        {filteredSops.map(sop => <li key={sop.id}>{sop.title}</li>)}
+      </ul>
+    ) : (
+      <p>No such data exists.</p>
+    )} */}
+            {/* {filteredData > 0 ?filteredData.data.map((sops,index) => { return(<Rowsop data={sops} key={index}  icon={setting}title={sops.title} description={sops.description}/>)}):
+           sop !== [] ? sop.data.map((sops,index )=>{
+            return ( <Rowsop data={sops} key={index} icon={setting}title={sops.title} description={sops.description}/>
+)              }):"no data" } */}
+   {/* {filteredData.data.map((sops,index) => <Rowsop data={sops} key={index} icon={setting}title={sops.title} description={sops.description}/>)} */}
+          
+              {  }
              
               {/* <Rowsop icon={check}/>
               <Rowsop icon={document}/> */}
             </div>
-            </div>
+           </div>
    </React.Fragment>
   )
 }
